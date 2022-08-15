0ebe4e1f524f4a9f20e53bf7c33d0860c3d5dcf1
milween <usualksm@gmail.com>
2022-08-15 15:11:07 +0200

    Add fetch / displayData recipes

---
+++ scripts/main_index/index.js
@@ -0,0 +1,25 @@
+async function getDataJson() {
+  const response = await fetch ('data/recipes.json');
+  const recipes = await response.json();
+  // console.log(recipes);
+
+  return recipes;
+}
+
+async function displayData(recipes) {
+  const recipeSection = document.getElementById('recipes__cards');
+  recipeSection.innerHTML = '';
+  for (const recipe of recipes) {
+
+    /* getRecipeCard is defined in recipes_cards.js */
+    const recipeCard = getRecipeCard(recipe);
+    recipeSection.appendChild(recipeCard);
+  }
+}
+
+async function init() {
+  const { recipes } = await getDataJson();
+  displayData(recipes);
+}
+
+init();
